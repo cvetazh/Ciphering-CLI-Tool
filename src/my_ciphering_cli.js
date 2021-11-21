a1e459caf28f486594bca7831fcde7c64ec676de
Svetlana Zhilina <sveta199951@gmail.com>
2021-11-21 22:27:55 +0300

    fix: add try\ catch  my_ciphering_cli

--- src/my_ciphering_cli.js
+++ src/my_ciphering_cli.js
@@ -1,7 +1,16 @@
 const { getValidConfig, getFile, isExistsOptional} = require('./consoleValidation.js');
+const { argv, exit } = process;
 const { transformation } = require('./transformation.js');
 const input = ['-i', '--input'];
 const output = ['-o', '--output'];
+const argvs = argv.slice(2);
 
-isExistsOptional();
-transformation(getValidConfig(), getFile(input), getFile(output))
+try{
+  isExistsOptional(argvs);
+  transformation(getValidConfig(argvs), getFile(argvs, input), getFile(argvs, output))
+}
+catch (error) {
+  console.error(error.message);
+  exit(1);
+}
+
