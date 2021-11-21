d0c883e7890f31160d43050732ca5c1e5b51f572
Svetlana Zhilina <sveta199951@gmail.com>
2021-11-21 13:28:16 +0300

    add a new testcase

--- _tests_/atbashCipher.test.js
+++ _tests_/atbashCipher.test.js
@@ -1,14 +1,26 @@
 const { atbashCipher } = require('../src/atbashCipher');
 
 describe('atbashCipher', () =>{
+
   test('should encode string by Atbash cipher ', () => {
     //Arrange
-    const str = 'abcdefghijklmnopqrstuvwxyzAZ';
+    const str = 'abcdefghijklmnopqrstuvwxyz A Z';
     //Act
     const encodedStr = atbashCipher(str);
     //Assert
-    expect(encodedStr).toBe('zyxwvutsrqponmlkjihgfedcbaZA')
+    expect(encodedStr).toBe('zyxwvutsrqponmlkjihgfedcba Z A')
     });
+
+  test('should Russian letters, numbers and special characters does NOT encode ', () => {
+    //Arrange
+    const str = 'йцукенгшщзхъфывапролджэячсмитьбю 123 _-?!",.';
+    //Act
+    const encodedStr = atbashCipher(str);
+    //Assert
+    expect(encodedStr).toBe('йцукенгшщзхъфывапролджэячсмитьбю 123 _-?!",.')
+    });
+
 });
+
 
 
